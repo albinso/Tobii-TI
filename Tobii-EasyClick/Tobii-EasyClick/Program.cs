d0cb54426e4697c7f2118c819f0ef60e98ca2ead
MisterWeasel <heerkip@gmail.com>
2015-03-25 09:53:29 +0100

    Vorking mouse clicks

--- Tobii-EasyClick/Tobii-EasyClick/Program.cs
+++ Tobii-EasyClick/Tobii-EasyClick/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Tobii_EasyClick
 {
     class Program
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
         static void Main(string[] args)
         {
+            MouseClick(1360, 10);
+        }
+
+        public static void MouseClick(uint x, uint y)
+        {
+            //SetCursorPos((int)x, (int)y);
+            Thread.Sleep(5000);
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, x, y, 0, 0);
         }
     }
 }
